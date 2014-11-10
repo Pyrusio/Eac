4acefde1a549c0f2c0019ce44fde58ede438d3b0
Abchrisabc <Chris@The-Eac.nl>
2014-11-10 10:11:21 +0100

    Deleted Channel Was never used and was equal to MOD_ID

--- src/main/java/Eac/reference/Reference.java
+++ src/main/java/Eac/reference/Reference.java
@@ -6,7 +6,6 @@ public class Reference
 	public static final String MOD_ID = "eac";
 	public static final String MOD_NAME = "Enders Aerial Cast";
 	public static final String VERSION = "v1.4.0-pre1";
-	public static final String CHANNEL = MOD_ID;
 //    public static final String GUI_FACTORY_CLASS = "Eac.client.gui.GuiFactory";
     public static final String CLIENT_PROXY_CLASS = "Eac.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "Eac.proxy.ServerProxy";
