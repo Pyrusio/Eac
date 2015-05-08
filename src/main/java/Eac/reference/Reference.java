b73ebe1bd53412f5ab951bd4161102fdd283ee67
abchrisabc <Chris@The-Eac.nl>
2015-05-08 16:58:41 +0200

    Change of version number

--- src/main/java/Eac/reference/Reference.java
+++ src/main/java/Eac/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 
 	public static final String MOD_ID = "eac";
 	public static final String MOD_NAME = "Enders Aerial Cast";
-	public static final String VERSION = "v1.4.0-pre2";
+	public static final String VERSION = "v2.0.0-pre1";
 //    public static final String GUI_FACTORY_CLASS = "Eac.client.gui.GuiFactory";
     public static final String CLIENT_PROXY_CLASS = "Eac.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "Eac.proxy.ServerProxy";
